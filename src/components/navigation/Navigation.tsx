e43cfc7b6d16b294f85871bd761a3dfb01035bce
Carlos David Gonzalez Nexans <cdgn.machines@gmail.com>
2023-11-14 17:45:10 -0300

    fix: start cv

---
+++ src/components/navigation/Navigation.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import Link from "next/link";
+
+export default function Navigation({ className }: { className?: string }) {
+  return (
+    <NavigationMenu className={className}>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <Link href="/about" legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              About
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link href="/articles" legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Articles
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+}
